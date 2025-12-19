eb978b08d59b9dcb9619cf2446e182d5fbe6ba13
secureshieldpim-tech <rosadocortes24>
2025-12-19 10:20:11 +0100

    Actualizacion v4 BUG FIX vista dentro de dashboard.php en dispositivos moviles

--- dashboard.php
+++ dashboard.php
@@ -14,6 +14,7 @@ $mensajes = $db->leerRegistros();
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - SecurityShield</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="css/style.css">
